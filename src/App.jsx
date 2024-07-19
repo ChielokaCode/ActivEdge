e6bde8885ceac7e138a1cab19360bd0410524fe0
ChielokaCode <realmatec01@gmail.com>
2024-07-19 20:57:22 +0100

    Get all Artiste
    Consumed Get all Artiste endpoint done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./output.css";
 
+const ArtisteList = React.lazy(() => import("./components/ArtisteList"));
+const UserProfile = React.lazy(() => import("./components/UserProfile"));
+const ArtisteAlbum = React.lazy(() => import("./components/ArtisteAlbum"));
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="m-4">
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex items-center justify-center h-[100vh]">
+                    Loading...
+                  </div>
+                }
+              >
+                <ArtisteList />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/user-profile/:userId"
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex items-center justify-center h-[100vh]">
+                    Loading...
+                  </div>
+                }
+              >
+                <UserProfile />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/user-album/:userId"
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex items-center justify-center h-[100vh]">
+                    Loading...
+                  </div>
+                }
+              >
+                <ArtisteAlbum />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ArtisteList.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ArtisteList = () => {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Define an async function to fetch the data
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    // Call the fetch function
+    fetchUsers();
+  }, []);
+
+  const handleUserProfile = (userId) => {
+    navigate(`/user-profile/${userId}`);
+  };
+  const handleUserAlbum = (userId) => {
+    navigate(`/user-album/${userId}`);
+  };
+
+  return (
+    <div className="space-y-3">
+      <h1 className="text-3xl font-bold">Artiste List</h1>
+      {users.map((user) => (
+        <div
+          key="{user.id}"
+          className="flex flex-col rounded-lg shadow-lg bg-gray-300 text-left cursor-pointer p-4"
+        >
+          {user.name}
+          <div className="flex justify-evenly">
+            <button
+              className="hover:scale-110 bg-green-300 p-4"
+              onClick={() => handleUserAlbum(user.id)}
+            >
+              View Album
+            </button>
+            <button
+              className="hover:scale-110 bg-green-300 p-4"
+              onClick={() => handleUserProfile(user.id)}
+            >
+              View Profile
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ArtisteList;
+
+
+ArtisteList
